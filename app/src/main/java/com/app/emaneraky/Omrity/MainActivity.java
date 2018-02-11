8d066d89052a4d51cca44faf87f34f6902103fc9
EmanEraky <https://github.com/emanmohamed0/Omrity>
2018-02-11 16:00:13 +0200

    AppLabikaOmra

--- app/src/main/java/com/app/emaneraky/Omrity/MainActivity.java
+++ app/src/main/java/com/app/emaneraky/Omrity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.emaneraky.Omrity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -7,11 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
-    TextView searchtxt, choice_place;
+    TextView searchtxt, choice_place, check_out, check_in;
     Button searchnow_btn;
+    Calendar from, to;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +29,19 @@ public class MainActivity extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        from = Calendar.getInstance();
+        to = Calendar.getInstance();
+        to.add(Calendar.DATE, 1);
+
         searchtxt = (TextView) findViewById(R.id.searchText);
         choice_place = (TextView) findViewById(R.id.choice_place);
+        check_out = (TextView) findViewById(R.id.check_out);
+        check_in = (TextView) findViewById(R.id.check_in);
         searchnow_btn = (Button) findViewById(R.id.search);
+
+        updateLabelfrom();
+        updateLabelto();
+
         Typeface face = Typeface.createFromAsset(getAssets(), "fonts/NABILA.TTF");
         searchtxt.setTypeface(face);
 
@@ -43,5 +60,66 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
             }
         });
+        final DatePickerDialog.OnDateSetListener datefrom = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                from.set(Calendar.YEAR, year);
+                from.set(Calendar.MONTH, monthOfYear);
+                from.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelfrom();
+            }
+
+        };
+        check_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new DatePickerDialog(MainActivity.this, datefrom, from
+                        .get(Calendar.YEAR), from.get(Calendar.MONTH),
+                        from.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        final DatePickerDialog.OnDateSetListener dateto = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                to.set(Calendar.YEAR, year);
+                to.set(Calendar.MONTH, monthOfYear);
+                to.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelto();
+            }
+
+        };
+        check_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, dateto, to
+                        .get(Calendar.YEAR), to.get(Calendar.MONTH),
+                        to.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabelfrom() {
+
+        String myFormat = "EEE, MMM d"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        check_out.setText("check_out \n " + sdf.format(from.getTime()));
+
+    }
+
+    private void updateLabelto() {
+
+        String myFormat = "EEE, MMM d"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        check_in.setText("check_in \n " + sdf.format(to.getTime()));
     }
 }
